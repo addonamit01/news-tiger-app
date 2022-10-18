50d8ce4c1b9414a18fc450c04de165e37d5b05ca
Amit Viramgami <addon.amit01@gmail.com>
2022-10-18 12:21:02 +0530

    App Title & Heading Dynamic Change Feature

--- src/components/News.js
+++ src/components/News.js
@@ -17,13 +17,18 @@ export class News extends Component {
         category: propTypes.string,
     }
 
-    constructor() {
-        super();
+    capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    constructor(props) {
+        super(props);
         this.state = {
             articles: [],
             loading: false,
             page: 1
         }
+        document.title = `${ this.capitalizeFirstLetter(this.props.category) } - NewsTiger`;
     }
 
     async updateNews() {
@@ -55,7 +60,7 @@ export class News extends Component {
     render() {
         return (
             <div className="container my-3">
-                <h1 className="text-center my-4">NewsTiger - Top Headlines</h1>
+                <h1 className="text-center my-4">NewsTiger - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
                 {this.state.loading && <Spinner/>}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
